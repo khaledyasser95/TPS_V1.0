75edd0e7589300ff974f4ce826d2904d5521d284
Icy <Khaledyasser95@gmail.com>
2018-11-30 19:34:41 +0200

    Revert "Revert "iini 2""
    This reverts commit 664ce2f5693cbf6163eccfd98c922fc863fdb4c5.

---
+++ Assets/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+
+    [SerializeField] float weaponSwitchTime;
+    Shooter[] weapons;
+    Shooter activeWeapon;
+    int currentWeaponIndex;
+    bool canFire;
+    Transform weaponHolster;
+
+    public Shooter ActiveWeapon
+    {
+        get
+        {
+            return activeWeapon;
+        }
+    }
+    private void Awake()
+    {
+
+        canFire = true;
+        weaponHolster = transform.Find("Weapons");
+        weapons = weaponHolster.GetComponentsInChildren<Shooter>();
+        
+        if (weapons.Length > 0)
+            Equip(0);
+    }
+    private void Update()
+    {
+        if (GameManager.Instance.InputController.MouseWheelDown)
+            SwitchWeapon(1);
+
+        if (GameManager.Instance.InputController.MouseWheelUp)
+            SwitchWeapon(-1);
+
+        ///To be done with SPRINTING WALKING
+        //if (GameManager.Instance.LocalPlayer.PlayerState.MoveState == PlayerState.EMoveState.SPRINTING);
+        //return;
+
+        if (!canFire)
+            return;
+        if (GameManager.Instance.InputController.Fire1)
+        {
+            activeWeapon.Fire();
+        }
+    }
+    void DeactivateWeapon()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].transform.SetParent(weaponHolster);
+            weapons[i].gameObject.SetActive(false);
+        }
+    }
+
+    void SwitchWeapon(int direction)
+    {
+        canFire = false;
+        currentWeaponIndex += direction;
+        if (currentWeaponIndex > weapons.Length - 1)
+            currentWeaponIndex = 0;
+        if (currentWeaponIndex < 0)
+            currentWeaponIndex = weapons.Length - 1;
+
+        GameManager.Instance.Timer.Add(() =>
+        {
+            Equip(currentWeaponIndex);
+        }, weaponSwitchTime);
+    }
+    void Equip(int index)
+    {
+        DeactivateWeapon();
+        canFire = true;
+        activeWeapon = weapons[index];
+        activeWeapon.Equip();
+        
+        weapons[index].gameObject.SetActive(true);
+
+    }
+}
---
+++ Assets/Assets/Shared/ML_Manager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ML_Manager  {
+
+    private GameObject gameObject ;
+
+    private static ML_Manager m_Instance;
+    
+    public static ML_Manager Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new ML_Manager();
+                
+                m_Instance.gameObject = GameObject.Find("Player");
+               
+            }
+            return m_Instance;
+        }
+    }
+
+    private static ML_Manager m_Instance2;
+
+    public static ML_Manager Instance2
+    {
+        get
+        {
+            if (m_Instance2 == null)
+            {
+                m_Instance2 = new ML_Manager();
+
+                m_Instance2.gameObject = GameObject.Find("Agent");
+            }
+            return m_Instance2;
+        }
+    }
+
+    private Player_ML m_Player_ML;
+    public Player_ML playerInput
+    {
+        get
+        {
+            if (m_Player_ML == null)
+            {
+                m_Player_ML = gameObject.GetComponent<Player_ML>();
+            }
+            return m_Player_ML;
+        }
+    }
+}
